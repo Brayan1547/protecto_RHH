75c2d4440d6c528125b0945e000044880c95fc3c
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-05-19 20:18:56 -0500

    se creo user.routes.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-const exprees = require( 'express');
+const express = require( 'express');
 
-const app = exprees();
+const app = express();
+
+
 
 app.get('/health', (req, res ) => {
     res.json({
         msg:'sitio funcionado '
     });
 });
+
+app.use('/users', require('./routes/user.routes.js'));
 
 app.listen(3000, ()=>{
     console.log(`server running on http://localhost:3000`)
